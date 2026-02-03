a4d3e4f35fc97c58dc2345345dd7d7078b129bac
federicoroldos <fede212yt@gmail.com>
2026-02-03 19:10:15 +0900

    github pages deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ramyeon-dictionary/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 })
